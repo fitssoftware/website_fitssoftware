b4d6ce820b16504a432d1aca4cfe46587c31b128
FLI <urchuin@hotmail.com>
2025-05-09 11:09:30 +0200

    - typo fixes - removed description text by work history

--- build/wp-content/uploads/uag-plugin/assets/0/uag-js-298.js
+++ build/wp-content/uploads/uag-plugin/assets/0/uag-js-298.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function(){ window.addEventListene
 	UAGBCounter.init( '.uagb-block-41d1251b', {"layout":"bars","heading":"Stakeholder management","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":75,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-f9ff1981', {"layout":"bars","heading":"Docker","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":65,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-8c6bc9da', {"layout":"bars","heading":"Github \/ Git","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":85,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
 	UAGBCounter.init( '.uagb-block-4937ccd4', {"layout":"bars","heading":"React","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":70,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
@@ -17,12 +23,12 @@ window.addEventListener( 'load', function() {
 	UAGBCounter.init( '.uagb-block-edaa0655', {"layout":"bars","heading":"UX\/UI","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":40,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-8c6bc9da', {"layout":"bars","heading":"Github \/ Git","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":85,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
-window.addEventListener( 'load', function() {
 	UAGBCounter.init( '.uagb-block-d0848399', {"layout":"bars","heading":"Machine learning","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
 	UAGBCounter.init( '.uagb-block-f2fc92b5', {"layout":"bars","heading":"Cloud computing","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":50,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-bccd8118', {"layout":"bars","heading":"SCRUM","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":90,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
  });
